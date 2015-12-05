e7df08d05977b781f86a3e395106f11157380749
labuser <labuser@nowhere.com>
2015-12-05 14:47:34 -0500

    inital app

---
+++ routes/gitnotify.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.post('/', function(req, res, next) {
+  res.send('respond with a resource');
+  console.log('got this from git...')
+  console.log(req.body);
+});
+
+module.exports = router;
+
+
